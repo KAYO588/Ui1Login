4ab0cc020e5a66b49fe884454bf86c63548074f4
Nikita <nikitanikita21780@yandex.ru>
2023-03-10 20:26:35 +0300

    Test
    Test

---
+++ XmlLogin/app/src/main/java/com/example/xmllogin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.xmllogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.xmllogin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun onClickLogin( view: View) {
+        intent = Intent(this, Login::class.java)
+        startActivity(intent)
+    }
+}
